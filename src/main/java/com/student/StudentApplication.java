6bf91c81d52acb3a4b7d8a0f401a52b717faa5e7
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2021-02-01 18:00:06 +0530

    Initial Values

--- src/main/java/com/student/StudentApplication.java
+++ src/main/java/com/student/StudentApplication.java
@@ -27,21 +27,21 @@ public class StudentApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-//		Student student = new Student("Bill","Gates",new Date(),"17","11","B");
-//		service.save(student);
-//		Student student1 = new Student("Dan","Lok",new Date(),"16","10","C");
-//		service.save(student1);
-//		Student student2 = new Student("Stephen","Hawken",new Date(),"16","11","A");
-//		service.save(student2);
-//		Student student3 = new Student("Baba","Rajnesh",new Date(),"18","10","A");
-//		service.save(student3);
-//		Student student4 = new Student("Wolf","Gupta",new Date(),"19","12","C");
-//		service.save(student4);
-//		Student student5 = new Student("Stephen","Chow",new Date(),"19","12","D");
-//		service.save(student5);
-//		Student student6 = new Student("Captain.","Blackadder",new Date(),"25","11","B");
-//		service.save(student6);
-//		
+		Student student = new Student("Bill","Gates",new Date(),"17","11","B");
+		service.save(student);
+		Student student1 = new Student("Dan","Lok",new Date(),"16","10","C");
+		service.save(student1);
+		Student student2 = new Student("Stephen","Hawken",new Date(),"16","11","A");
+		service.save(student2);
+		Student student3 = new Student("Baba","Rajnesh",new Date(),"18","10","A");
+		service.save(student3);
+		Student student4 = new Student("Wolf","Gupta",new Date(),"19","12","C");
+		service.save(student4);
+		Student student5 = new Student("Stephen","Chow",new Date(),"19","12","D");
+		service.save(student5);
+		Student student6 = new Student("Captain.","Blackadder",new Date(),"25","11","B");
+		service.save(student6);
+		
 		
 		
 		
